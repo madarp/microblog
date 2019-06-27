b67bf916863d178f145b4b076205d3462ce72f50
Piero Madar <madarp@pobox.com>
2019-06-27 17:13:51 -0400

    Initial DB iimplementation and migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-
+# All of these are necessary globals
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # needed by `flask db` cli utility
 
-from app import routes
+from app import routes, models
--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+default_db = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_db
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
